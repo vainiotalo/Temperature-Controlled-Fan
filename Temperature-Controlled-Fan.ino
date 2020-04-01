3ee902b569b907dc431b0e4ff050ee5e19f23c64
pevain <pevain@utu.fi>
2020-04-01 23:42:28 +0300

    Initial commit

---
+++ Temperature-Controlled-Fan.ino
@@ -0,0 +1,46 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+/*
+Libraries available at
+https://github.com/PaulStoffregen/OneWire
+https://github.com/milesburton/Arduino-Temperature-Control-Library
+
+Tempereture Controlled ON/OFF fan using a 2-wire 12V DC fan and DS18B20 temperature sensor
+*/
+
+#define ONE_WIRE_BUS 5 // Set up sensor data on digital input 5
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+int Relay = 7; // Relay pin
+float temp = 0;
+float tempMax = 25.0; // Switch on the relay
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Relay, OUTPUT); // Used relay is active-low
+  digitalWrite(Relay, HIGH); // Relay switched off by default using input HIGH
+  sensors.begin();
+}
+
+void loop() {
+  temp = readTemp();
+  delay(1000);
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(" C \n");
+  if(temp > tempMax) { // If temp is higher than tempMax,
+    digitalWrite(Relay, LOW); // switch on the relay
+  } else {
+    digitalWrite(Relay, HIGH);
+  }
+}
+
+float readTemp(){
+  sensors.requestTemperatures(); // Request temperature from sensor
+  temp = sensors.getTempCByIndex(0); // Temperature in Celsius
+  return temp;
+}
